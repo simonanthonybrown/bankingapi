3acb60a7053809e5d79b9945388915415fe0ab17
Simon Brown <simonbrown@kubrickgroup.com>
2024-01-15 09:56:18 +0000

    put db_create.py back into a functional form

--- db_create.py
+++ db_create.py
@@ -6,26 +6,24 @@ con = sqlite3.connect("banking.db")
 # Create cursor connection
 cur = con.cursor()
 
-# cur.execute("CREATE TABLE accounts(account_num, sort_code, user_id, balance)")
+cur.execute("CREATE TABLE accounts(account_num, sort_code, user_id, balance)")
 
-# Check that database has been created
-# result = cur.execute("SELECT name FROM sqlite_master")
-# print(result.fetchone())
-
-# Account data to insert into db
-# acc_data = [
-#             (82041626, 927837, 'KL9283', 11500.23),
-#             (64736641, 057361, 'SY6627', 543.65),
-#             (70231442, 463782, 'PK2873', 8954.92),
-#             (44893061, 937478, 'WE7112', 34534.87),
-# ]
+# # Check that database has been created
+result = cur.execute("SELECT name FROM sqlite_master")
+print(result.fetchone())
 
 # Add values to the accounts table
-# cur.execute("INSERT INTO accounts VALUES(?, ?, ?, ?)", acc_data)
+cur.execute("""
+            INSERT INTO accounts VALUES
+                (82041626, 927837, 'KL9283', 11500.23),
+                (64736641, 560743, 'SY6627', 543.65),
+                (70231442, 463782, 'PK2873', 8954.92),
+                (44893061, 937478, 'WE7112', 34534.87)
+""")
 
 # Commit the changes
-# con.commit()
+con.commit()
 
 # Check that values were entered into the db
-# result = cur.execute("SELECT account_num FROM accounts")
-# print(result.fetchall())
+result = cur.execute("SELECT account_num FROM accounts")
+print(result.fetchall())
